6c75eb03f7a46fd7f96593fd99d44c5874171aad
Henry <>
2024-09-04 10:06:30 +0100

    initial commit

---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,66 @@
+import { router } from "expo-router";
+import {View, Text, TouchableOpacity, Image} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Swiper from "react-native-swiper"
+
+import { useRef, useState } from "react";
+import {onboarding} from "@/constants";
+import CustomButton from "@/components/CustomButton"
+
+const Onboarding = () => {
+    const swiperRef = useRef<Swiper>(null);
+    const [activeIndex, setActiveIndex] = useState(0);
+    const isLastSlide = activeIndex === onboarding.length -1;
+    
+    return (
+      <SafeAreaView className="flex h-full items-center justify-between bg-white">
+        <TouchableOpacity
+          onPress={() => {
+            router.replace("/(auth)/sign-up");
+          }}
+          className="w-full flex justify-end items-end p-3"
+        >
+          <Text className="text-black text-md font-JakartaBold">Skip</Text>
+        </TouchableOpacity>
+
+        <Swiper
+          ref={swiperRef}
+          loop={false}
+          dot={<View className="w-8 h-1 mx-1 bg-[#E2E8F0] rounded-full" />}
+          activeDot={
+            <View className="w-8 h-1 mx-1 bg-[#0286FF] rounded-full" />
+          }
+          onIndexChanged={(index) => setActiveIndex(index)}
+        >
+          {onboarding.map((item) => (
+            <View key={item.id} className="items-center justify-center p-5">
+              <Image
+                source={item.image}
+                className="w-full h-[300px]"
+                resizeMode="contain"
+              />
+              <View className="flex-row items-center justify-center w-full">
+                <Text className="text-black text-3xl font-bold mx-10 text-center">
+                  {item.title}
+                </Text>
+              </View>
+              <Text className="text-lg text-center font-JakartaSemibold text-[#858585] mx-10 mt-3">
+                {item.description}
+              </Text>
+            </View>
+          ))}
+        </Swiper>
+
+        <CustomButton 
+            title={isLastSlide ? "Get Started" : "Next"} 
+            className="w-11/12" 
+            onPress={() => 
+                isLastSlide ? router.replace('/(auth)/sign-up')
+                : swiperRef.current?.scrollBy(1)
+            }
+        />
+      </SafeAreaView>
+    );
+}
+
+export default Onboarding;
---
+++ app/(root)/(tabs)/home.tsx
@@ -0,0 +1,28 @@
+import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
+import { Link } from "expo-router";
+import { View, Text } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Home = () => {
+    const {user} = useUser();
+    
+    return (
+        <SafeAreaView>
+            <View>
+                <SignedIn>
+                    <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+                </SignedIn>
+                <SignedOut>
+                    <Link href="/sign-in">
+                      <Text>Sign In</Text>
+                    </Link>
+                    <Link href="/sign-up">
+                      <Text>Sign Up</Text>
+                    </Link>
+                </SignedOut>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default Home;
